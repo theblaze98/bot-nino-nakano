5fd2de693f4e0bcdf596b8cda374b07cde9aa5f9
theblaze98 <dionijvargas@gmail.com>
2023-01-19 02:24:06 -0400

    Finished prefix command handler

--- index.js
+++ index.js
@@ -31,4 +31,51 @@ client.on('ready', () => {
 	console.log(`El bot ${client.user.tag} esta activo`.green);
 });
 
+client.on('messageCreate', (message) => {
+	if (
+		message.author === 'bot' ||
+		!message.guild ||
+		message.channel.type === 'dm'
+	)
+		return;
+
+	if (!message.content.startsWith(config.prefix)) return;
+
+	let args = message.content.slice(config.prefix.length).trim().split(/ +/);
+	let command = args.shift().toLowerCase();
+
+	let cmd = client.commands.find(
+		(c) => c.name === command || c.alias.includes(command)
+	);
+
+	if (cmd) cmd.run(client, message, args, color());
+});
+
 client.login(config.token);
+
+/* ------------------------ */
+/* Prefix Command Handler */
+/* ------------------------ */
+
+client.commands = new Collection();
+
+console.log(`Prefix Commands`.red);
+readdirSync('./commands').forEach((dir) => {
+	const commands = readdirSync(`./commands/${dir}/`).filter((file) =>
+		file.endsWith('.js')
+	);
+	for (const file of commands) {
+		const command = require(`./commands/${dir}/${file}`);
+		console.log(`✅ - Comando ${file.replace('.js', '')} cargado`.cyan);
+		client.commands.set(command.name, command);
+	}
+});
+
+function color() {
+	const valores = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+		color += valores[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
