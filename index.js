7cdc768ef0199a402bf120ac2d9b76198cdc7004
theblaze98 <dionijvargas@gmail.com>
2023-01-29 19:48:04 -0400

    Cambios al index.js

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const {
 	GatewayIntentBits,
 	Partials,
 	Collection,
-} = require('discord.js');
+	ActivityType,
+	PresenceUpdateStatus,
+} = require( 'discord.js' );
 
-const client = new Client({
+const client = new Client( {
 	intents: [
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMembers,
@@ -20,18 +22,31 @@ const client = new Client({
 	],
 	allowedMentions: {
 		repliedUser: false,
-		parse: ['users', 'roles'],
+		parse: [ 'users', 'roles' ],
 	},
-});
-require('colors');
-const { readdirSync } = require('fs');
-const config = require('./config.json');
+	presence: {
+		activities: [
+			{ name: 'a mi hermoso creador', type: ActivityType.Watching },
+		],
+		status: PresenceUpdateStatus.Online,
+	},
+} );
+require( 'colors' );
+const { readdirSync } = require( 'fs' );
+const config = require( './config.json' );
 
-client.on('ready', () => {
-	console.log(`El bot ${client.user.tag} esta activo`.green);
-});
+client.commands = new Collection();
+client.slashcommands = new Collection();
 
-client.on('messageCreate', (message) => {
+client.on( 'ready', () =>
+{
+	console.log( `El bot ${client.user.tag} esta activo`.green );
+
+	client.application.commands.set( client.slashcommands.map( ( x ) => x ) );
+} );
+
+client.on( 'messageCreate', ( message ) =>
+{
 	if (
 		message.author === 'bot' ||
 		!message.guild ||
@@ -39,43 +54,71 @@ client.on('messageCreate', (message) => {
 	)
 		return;
 
-	if (!message.content.startsWith(config.prefix)) return;
+	if ( !message.content.startsWith( config.prefix ) ) return;
 
-	let args = message.content.slice(config.prefix.length).trim().split(/ +/);
+	let args = message.content.slice( config.prefix.length ).trim().split( / +/ );
 	let command = args.shift().toLowerCase();
 
 	let cmd = client.commands.find(
-		(c) => c.name === command || c.alias.includes(command)
+		( c ) => c.name === command || c.alias.includes( command )
 	);
 
-	if (cmd) cmd.run(client, message, args, color());
-});
+	if ( cmd ) cmd.run( client, message, args, color() );
+} );
 
-client.login(config.token);
+client.on( 'interactionCreate', ( int ) =>
+{
+	const cmd = client.slashcommands.get( int.commandName );
+	if ( !cmd ) return;
+	cmd.execute( client, int, color() );
+} );
+
+client.login( config.token );
 
 /* ------------------------ */
 /* Prefix Command Handler */
 /* ------------------------ */
 
-client.commands = new Collection();
-
-console.log(`Prefix Commands`.red);
-readdirSync('./commands').forEach((dir) => {
-	const commands = readdirSync(`./commands/${dir}/`).filter((file) =>
-		file.endsWith('.js')
+console.log( `Prefix Commands`.red );
+readdirSync( './commands' ).forEach( ( dir ) =>
+{
+	const commands = readdirSync( `./commands/${dir}/` ).filter( ( file ) =>
+		file.endsWith( '.js' )
 	);
-	for (const file of commands) {
-		const command = require(`./commands/${dir}/${file}`);
-		console.log(`✅ - Comando ${file.replace('.js', '')} cargado`.cyan);
-		client.commands.set(command.name, command);
+	for ( const file of commands )
+	{
+		const command = require( `./commands/${dir}/${file}` );
+		console.log( `✅ - Comando ${file.replace( '.js', '' )} cargado`.cyan );
+		client.commands.set( command.name, command );
 	}
-});
+} );
 
-function color() {
+/* ------------------------ */
+/* Slash Command Handler */
+/* ------------------------ */
+
+console.log( `Slash Commands`.red );
+
+readdirSync( './slashcommands' ).forEach( ( dir ) =>
+{
+	const slashcommands = readdirSync( `./slashcommands/${dir}` ).filter( ( file ) =>
+		file.endsWith( '.js' )
+	);
+	for ( const file of slashcommands )
+	{
+		const slashcommand = require( `./slashcommands/${dir}/${file}` );
+		console.log( `✅ - Comando ${file.replace( '.js', '' )} cargado`.cyan );
+		client.slashcommands.set( slashcommand.name, slashcommand );
+	}
+} );
+
+function color ()
+{
 	const valores = '0123456789ABCDEF';
 	let color = '#';
-	for (let i = 0; i < 6; i++) {
-		color += valores[Math.floor(Math.random() * 16)];
+	for ( let i = 0; i < 6; i++ )
+	{
+		color += valores[ Math.floor( Math.random() * 16 ) ];
 	}
 	return color;
 }
